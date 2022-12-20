fc225f754e021f0f34402375b179813a39f1d036
mac-DavidVilla <mac-davidvilla@MacBook-Pro-de-autentia.local>
2022-12-20 16:50:14 +0100

    test

---
+++ src/main/java/com/demo/hook/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.demo.hook.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class HelloWorldController {
+
+    @GetMapping
+    public String findAllPayments(){
+        return "Hello world test!";
+    }
+
+}
