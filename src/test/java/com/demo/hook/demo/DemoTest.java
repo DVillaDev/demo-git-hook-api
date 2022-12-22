023a89f3a2c534b49ac74f77275ee6f58a3462d8
DVillaDev <david.villa@autentia.com>
2022-12-22 11:32:39 +0100

    Add test.

---
+++ src/test/java/com/demo/hook/demo/DemoTest.java
@@ -0,0 +1,14 @@
+package com.demo.hook.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoTest {
+
+    @Test
+    public void testConcatenate() {
+        assertEquals(true, true);
+    }
+
+}
