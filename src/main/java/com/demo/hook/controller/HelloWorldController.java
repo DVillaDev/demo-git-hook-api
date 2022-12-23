fc325eed07169179003b426a7ae43c65efa29e33
luismerinou <lmerinoulizarna@gmail.com>
2022-12-23 08:39:57 +0100

    new version api #2

--- src/main/java/com/demo/hook/controller/HelloWorldController.java
+++ src/main/java/com/demo/hook/controller/HelloWorldController.java
@@ -17,7 +17,7 @@ public class HelloWorldController {
     @GetMapping
     @RequestMapping("/bye")
     public String byeWorld(){
-        return "Bye my world!";
+        return "Bye again my world!";
     }
 
 }
